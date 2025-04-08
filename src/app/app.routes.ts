5e8527ffe74be5111596201a961cd4c1668984e7
Dairo Barrios Ramos <74967610+dairo1992@users.noreply.github.com>
2025-04-08 13:18:05 -0500

    pokemon ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about-page/about-page.component')
+  },
+  {
+    path: 'price',
+    loadComponent: () => import('./pages/price-page/price-page.component')
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact-page/contact-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: () => {
+      // const authService = inject(AuthService)
+      return 'about';
+    }
+  }
+];
---
+++ src/app/pages/price-page/price-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'price-page',
+  imports: [],
+  templateUrl: './price-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricePageComponent { }
